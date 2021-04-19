c7112388c5fe13ae8fe9b40dc150de8294e03f30
Shea Cronin <shealcronin@gmail.com>
2021-04-19 18:24:48 -0400

    Convert weatherFactory to Weather class and add temp properties for each temperature unit and a method to convert between units

--- src/dom-weather.js
+++ src/dom-weather.js
@@ -19,9 +19,9 @@ function updateWeatherInfo(weatherObj) {
     weatherInfo.location.textContent = weatherObj.location;
     weatherInfo.desc.textContent = weatherObj.desc;
     weatherInfo.icon.src = icons[weatherObj.iconName];
-    weatherInfo.temp.textContent = weatherObj.temp;
-    weatherInfo.highTemp.textContent = weatherObj.highTemp;
-    weatherInfo.lowTemp.textContent = weatherObj.lowTemp;
+    weatherInfo.temp.textContent = weatherObj.temp.f;
+    weatherInfo.highTemp.textContent = weatherObj.temp.high.f;
+    weatherInfo.lowTemp.textContent = weatherObj.temp.low.f;
 }
 
 export { updateWeatherInfo };
--- src/weather.js
+++ src/weather.js
@@ -14,14 +14,63 @@ async function getWeather(location) {
     }
 }
 
-const weatherFactory = (location, temp, lowTemp, highTemp, desc, iconName) => {
-    return { location, temp, lowTemp, highTemp, desc, iconName };
-};
+class Weather {
+    constructor(location, temp, lowTemp, highTemp, desc, iconName) {
+        this.temp = {
+            k: Math.round(temp),
+            f: Math.round(((temp - 273.15) * 9) / 5 + 32),
+            c: Math.round(temp - 273.15),
+            low: {
+                k: Math.round(lowTemp),
+                f: Math.round(((lowTemp - 273.15) * 9) / 5 + 32),
+                c: Math.round(lowTemp - 273.15),
+            },
+            high: {
+                k: Math.round(highTemp),
+                f: Math.round(((highTemp - 273.15) * 9) / 5 + 32),
+                c: Math.round(highTemp - 273.15),
+            },
+        };
+        this.tempUnit = 'K';
+        this.location = location;
+        this.desc = desc;
+        this.iconName = iconName;
+    }
+    convertTemp(newUnit) {
+        switch (this.tempUnit) {
+            case 'K':
+                if (newUnit === 'F') {
+                    this.temp.k = ((this.temp.k - 273.15) * 9) / 5 + 32;
+                } else if (newUnit === 'C') {
+                    this.temp.k -= 273.15;
+                }
+                break;
+            case 'F':
+                if (newUnit === 'K') {
+                    this.temp.k = ((this.temp.k - 32) * 5) / 9 + 273.15;
+                } else if (newUnit === 'C') {
+                    this.temp.k = ((this.temp.k - 32) * 5) / 9;
+                }
+                break;
+            case 'C':
+                if (newUnit === 'K') {
+                    this.temp.k += 273.15;
+                } else if (newUnit === 'F') {
+                    this.temp.k = (this.temp.k * 9) / 5 + 32;
+                }
+                break;
+            default:
+                console.log('Invalid temperature unit.');
+        }
+        this.tempUnit = newUnit;
+        return this.temp.k;
+    }
+}
 
 async function processWeatherData(location) {
     const data = await getWeather(location);
     console.log(data);
-    const weather = weatherFactory(
+    const weather = new Weather(
         data.name,
         data.main.temp,
         data.main['temp_min'],
