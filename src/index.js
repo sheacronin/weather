694ebc2963b812ee507d664fd62edef16de34607
Shea Cronin <shealcronin@gmail.com>
2021-04-19 11:47:43 -0400

    Wrap input and button in form element and clear input value and preventDefault on submit

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ processWeatherData('London,uk').then((weather) => {
 const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('button');
 
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener('click', (e) => {
     processWeatherData(searchInput.value).then((weather) => {
         updateWeatherInfo(weather);
     });
+    searchInput.value = '';
+    e.preventDefault();
 });
