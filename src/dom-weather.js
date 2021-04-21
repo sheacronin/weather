f50693f51a36f24b407abb5b192bd7a435073bee
Shea Cronin <shealcronin@gmail.com>
2021-04-21 11:46:35 -0400

    Add initial display of degrees units to temperature

--- src/dom-weather.js
+++ src/dom-weather.js
@@ -15,13 +15,19 @@ const weatherInfo = {
     lowTemp: document.querySelector('#low-temp'),
 };
 
+function displayTemperature(tempType, temp, unit) {
+    weatherInfo[tempType].querySelector('.degrees-num').textContent = temp;
+    weatherInfo[tempType].querySelector('.degrees-unit').textContent =
+        '°' + unit;
+}
+
 function updateWeatherInfo(weatherObj) {
     weatherInfo.location.textContent = weatherObj.location;
     weatherInfo.desc.textContent = weatherObj.desc;
     weatherInfo.icon.src = icons[weatherObj.iconName];
-    weatherInfo.temp.textContent = weatherObj.temp.f;
-    weatherInfo.highTemp.textContent = weatherObj.temp.high.f;
-    weatherInfo.lowTemp.textContent = weatherObj.temp.low.f;
+    displayTemperature('temp', weatherObj.temp.f, 'F');
+    displayTemperature('highTemp', weatherObj.temp.high.f, 'F');
+    displayTemperature('lowTemp', weatherObj.temp.low.f, 'F');
 }
 
 export { updateWeatherInfo };
--- src/weather.js
+++ src/weather.js
@@ -16,48 +16,33 @@ async function getWeather(location) {
 
 class Weather {
     constructor(location, temp, lowTemp, highTemp, desc, iconName) {
+        this.tempUnit = 'F';
         this.temp = {
-            k: Math.round(temp),
+            //k: Math.round(temp),
             f: Math.round(((temp - 273.15) * 9) / 5 + 32),
             c: Math.round(temp - 273.15),
             low: {
-                k: Math.round(lowTemp),
+                //k: Math.round(lowTemp),
                 f: Math.round(((lowTemp - 273.15) * 9) / 5 + 32),
                 c: Math.round(lowTemp - 273.15),
             },
             high: {
-                k: Math.round(highTemp),
+                //k: Math.round(highTemp),
                 f: Math.round(((highTemp - 273.15) * 9) / 5 + 32),
                 c: Math.round(highTemp - 273.15),
             },
         };
-        this.tempUnit = 'K';
         this.location = location;
         this.desc = desc;
         this.iconName = iconName;
     }
     convertTemp(newUnit) {
         switch (this.tempUnit) {
-            case 'K':
-                if (newUnit === 'F') {
-                    this.temp.k = ((this.temp.k - 273.15) * 9) / 5 + 32;
-                } else if (newUnit === 'C') {
-                    this.temp.k -= 273.15;
-                }
-                break;
             case 'F':
-                if (newUnit === 'K') {
-                    this.temp.k = ((this.temp.k - 32) * 5) / 9 + 273.15;
-                } else if (newUnit === 'C') {
-                    this.temp.k = ((this.temp.k - 32) * 5) / 9;
-                }
+                this.temp.k = ((this.temp.k - 32) * 5) / 9;
                 break;
             case 'C':
-                if (newUnit === 'K') {
-                    this.temp.k += 273.15;
-                } else if (newUnit === 'F') {
-                    this.temp.k = (this.temp.k * 9) / 5 + 32;
-                }
+                this.temp.k = (this.temp.k * 9) / 5 + 32;
                 break;
             default:
                 console.log('Invalid temperature unit.');
